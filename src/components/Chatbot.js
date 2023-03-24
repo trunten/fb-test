7b39691044b6f2875cba4a018a9685eaa449d056
Jonathan Davis <trunten@btinternet.com>
2023-03-24 13:02:23 +0000

    weather bot

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -4,7 +4,7 @@ const configuration = new Configuration({
     apiKey: process.env.REACT_APP_OPENAI_API_KEY,
 });
 
-export default async function Chatbot(query, callback) {
+export async function Chatbot(query, callback) {
   if (!query) { return; }
   query = query.replace(/@bot/ig, "");
   const openai = new OpenAIApi(configuration);
@@ -15,4 +15,15 @@ export default async function Chatbot(query, callback) {
   });
   const text = completion.data.choices[0].message.content;
   callback(text);
+}
+
+export async function WeatherBot(callback) {
+  const key ="2ZQRSVF6JFPA239FUT6U6WBL5";
+  const url = "https://api.bigdatacloud.net/data/reverse-geocode-client";
+  const response = await fetch(url)
+  const { latitude, longitude } = await response.json();
+  const weatherResponse = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude},${longitude}?unitGroup=metric&key=${key}&contentType=json`)
+  const weather = await weatherResponse.json()
+  const { temp, icon, conditions } = weather.currentConditions;
+  return { temp, icon, conditions };
 }
