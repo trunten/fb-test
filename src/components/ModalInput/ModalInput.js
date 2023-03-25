c8a9ac7b4266cedce65603a83f96954ad68d2282
samuel_tuki <smauel_tuki@yahoo.com>
2023-03-25 14:26:20 +0000

    added modal template

--- src/components/ModalInput/ModalInput.js
+++ src/components/ModalInput/ModalInput.js
@@ -7,5 +7,22 @@ export default function ModalInput({ title, prompt, placeholder, submit }) {
   const inputValue = useRef(0);
 
   return (
-  )  
+    <>
+      {/* <button onClick={() => setOpen(true)}>Open sheet</button> */}
+      <SiWindows11 onClick={() => setOpen(true)} className="icon-button" />
+
+      <Sheet
+        isOpen={isOpen}
+        onClose={() => setOpen(false)}
+        detent="content-height"
+      >
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content></Sheet.Content>
+        </Sheet.Container>
+
+        <Sheet.Backdrop />
+      </Sheet>
+    </>
+  );
 }
