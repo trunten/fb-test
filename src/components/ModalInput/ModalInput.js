1f3429a86427b6b16864e2279dc4894ed667c1da
samuel_tuki <smauel_tuki@yahoo.com>
2023-03-25 14:23:20 +0000

    import setState,added proms and button icon

--- src/components/ModalInput/ModalInput.js
+++ src/components/ModalInput/ModalInput.js
@@ -1,1 +1,5 @@
-export default function ModalInput() {}
+import Sheet from "react-modal-sheet";
+import { useState, useRef } from "react";
+import { SiWindows11 } from "react-icons/si";
+
+export default function ModalInput({ title, prompt, placeholder, submit }) {}
