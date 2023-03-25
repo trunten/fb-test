0eb39986dac070f389b8ed9af51a3b0f217479e3
samuel_tuki <smauel_tuki@yahoo.com>
2023-03-25 14:25:09 +0000

    set state and set reference to input value

--- src/components/ModalInput/ModalInput.js
+++ src/components/ModalInput/ModalInput.js
@@ -2,4 +2,10 @@ import Sheet from "react-modal-sheet";
 import { useState, useRef } from "react";
 import { SiWindows11 } from "react-icons/si";
 
-export default function ModalInput({ title, prompt, placeholder, submit }) {}
+export default function ModalInput({ title, prompt, placeholder, submit }) {
+  const [isOpen, setOpen] = useState(false);
+  const inputValue = useRef(0);
+
+  return (
+  )  
+}
