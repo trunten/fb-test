1793c16746c675ce64c107284fa69265b91c7851
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-03-26 23:53:23 +0100

    editing intro text

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -12,7 +12,7 @@ export default function LandingPage() {
           <div className="img-container">
             <img src={chatIcon} alt="chat icon" className="chat-icon" />
           </div>
-          <h4>Welcome to ChatterBox</h4>
+          <h4><span className="intro-span">Welcome to</span> <br/> ChatterBox</h4>
           <div>
             <SignIn />
           </div>
