ec99eea70629d1621c2b16a82851a3e98fb35b1b
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-03-26 23:48:43 +0100

    changed landing page icon

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,42 +1,16 @@
 import "./LandingPage.css";
 import { SignIn } from "../Auth";
-import { useState, useEffect, useRef } from "react";
+
 import chatIcon from "../../images/chat.png";
 
 export default function LandingPage() {
-  const [loading, setLoading] = useState("loading");
-  const imgRef = useRef(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading("");
-    }, 500);
-  }, []);
-
-  const handleMouseEnter = () => {
-    imgRef.current.classList.add("glow");
-  };
-
-  const handleMouseLeave = () => {
-    imgRef.current.classList.remove("glow");
-  };
-
   return (
-    <div id="splash" className={loading}>
+    <div id="splash">
       <div className="gradient-overlay"></div>
       <div className="card">
         <div className="card-body">
-          <div
-            className="img-container"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <img
-              src={chatIcon}
-              alt="chat icon"
-              className="chat-icon"
-              ref={imgRef}
-            />
+          <div className="img-container">
+            <img src={chatIcon} alt="chat icon" className="chat-icon" />
           </div>
           <h4>Welcome to ChatterBox</h4>
           <div>
