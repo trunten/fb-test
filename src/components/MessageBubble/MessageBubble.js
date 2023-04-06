349ffe3fd4f99779df89295b1ed365de22911c03
Jonathan Davis <trunten@btinternet.com>
2023-04-06 20:01:15 +0100

    long message fix

--- src/components/MessageBubble/MessageBubble.js
+++ src/components/MessageBubble/MessageBubble.js
@@ -105,7 +105,7 @@ export default function MessageBubble({ message }) {
         <div className="img-wrapper" style={{position:"relative"}} data-initial={imgError ? initial : ""} >
         <img src={isBot ? bot : (imgError ? avatar : photoURL)} alt={initial || "avatar"} onError={()=>setImgError(true)} />
         </div>
-        <p style={{display:"flex", alignItems:"center", gap:"10px"}}>
+        <p style={{display:message.icon?"flex":"", alignItems:"center", gap:"10px"}}>
         {message.icon && <><img style={{margin:"auto", backgroundColor:"inherit", borderRadius:"0", width:"auto", height:"25px"}} src={`${process.env.PUBLIC_URL}/weather/${message.icon}.svg`} alt="weather conditions" onError={(e)=>e.target.remove()} /></>}<div style={{whiteSpace:"pre-wrap"}}><Linkify>{text}</Linkify></div>
         </p>
       </div>
